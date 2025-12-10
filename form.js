97459b9a0a33dc85731804f30d9fc8570595ae60
Milena Ues <milenaues@hotmail.com>
2025-12-10 20:03:22 -0300

    commit

--- form.js
+++ form.js
@@ -4,5 +4,5 @@ const form = document.getElementById("form");
 window.addEventListener("submit", (e) => {
     let nome = document.getElementById("nome");
     e.preventDefault;
-    alert("Salve"+ nome.value + "Obrigado por entrar em contato conosco!");
+    alert("Salve "+ nome.value + ", Obrigado por entrar em contato conosco!");                          
 })
